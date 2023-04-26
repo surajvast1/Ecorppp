800f83e6e1785246e4ddd932bdd20b778444a0eb
Shubham <srivastavashubham2003@gmail.com>
2023-04-26 12:58:33 +0530

    Added footers

--- nerko/src/layout/footers/footer.js
+++ nerko/src/layout/footers/footer.js
@@ -1,6 +1,8 @@
 import { Link } from "gatsby";
 import React from "react";
 import { animateScroll as scroll } from 'react-scroll';
+import './footer.css';
+
 
 // handle scroll to top
 const handleScrollToTop = () => {
@@ -9,108 +11,137 @@ const handleScrollToTop = () => {
 
 const Footer = () => {
   return (
-    <footer className="footer-area section-py-80">
+    <>
+  <div className="bg-black">
+    
       <div className="container">
-        <div className="footer__wrapper">
-          <img
-            src="/assets/img/objects/ethereum-01.png"
-            width="32"
-            alt="object"
-            style={{ top: '32%', left: '16%' }}
-          />
-          <img
-            src="/assets/img/objects/x.png"
-            width="16"
-            alt="object"
-            style={{ top: '8%', right: '16%' }}
-          />
-          <img
-            src="/assets/img/objects/circle-01.png"
-            width="16"
-            alt="object"
-            style={{ bottom: '24%', right: '40%' }}
-          />
-          <img
-            src="/assets/img/objects/circle-03.png"
-            width="24"
-            alt="object"
-            style={{ bottom: '-8%', left: '30%' }}
-          />
-          <div className="row justify-content-center">
-            <div className="col-xl-5 col-lg-7 col-md-9 col-sm-11">
-              <div className="footer__info text-center">
-                <div className="footer-logo">
-                  <Link to="/">
-                    <img
-                      src="/assets/img/logo/nerko.svg"
-                      width="200"
-                      alt="Nerko"
-                    />
-                  </Link>
-                </div>
-                <p>
-                  We make it easy to Discover, Invest and manage all your NFTs
-                  at one place.
-                </p>
-                <ul className="list-wrap footer__social">
-                  <li>
-                    <a href="#">
-                      <i className="fab fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i className="fab fa-discord"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i className="fab fa-instagram"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i className="fab fa-telegram-plane"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="copyright__wrapper">
-            <div className="row">
-              <div className="col-md-6">
-                <div className="copyright__text">
-                  <p>&copy; {new Date().getFullYear()} Nerko. All rights reserved.</p>
-                </div>
-              </div>
-              <div className="col-md-6">
-                <div className="copyright__menu">
-                  <ul className="list-wrap">
-                    <li>
-                      <a href="#">Privacy policy</a>
-                    </li>
-                    <li>
-                      <a href="#">Terms of use</a>
-                    </li>
-                    <li className="backTop">
-                      <button
-                        onClick={handleScrollToTop}
-                        style={{ background: "transparent", border: "none" }}
-                        className="scroll-to-target"
-                        data-target="html"
-                      >
-                        <i className="flaticon-arrowhead-up"></i>
-                      </button>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+      <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top box">
+        
+
+        <div className="col mb-3 ">
+          <h5>START A BUSINESS</h5>
+          <ul className="nav flex-column">
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0 ">
+              Partnership Firm Registration
+              </a>
+            </li> 
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Section 8 Company Registration
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Farmer Producer Company
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Nidhi Company Registration
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Startup India Registration
+              </a>
+            </li>
+          </ul>
         </div>
-      </div>
-    </footer>
+        <div className="col mb-3">
+          <h5>TRADEMARK & COPYRIGHT</h5>
+          <ul className="nav flex-column">
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              International Trademark Registration
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              USA Trademark Registration
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Trademark Renewal
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Trademark Assignment
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Trademark Watch
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col mb-3">
+          <h5>CONVERT/CHANGE IN BUSINESS</h5>
+          <ul className="nav flex-column">
+            
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Partnership to LLP
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Private to One Person Company
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Private to Public Limited Company
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Change Objective of Business
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col mb-3">
+          <h5>LEGAL DOCUMENTATION</h5>
+          <ul className="nav flex-column">
+            
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Term Sheet Agreement
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Share Purchase Agreement
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Non Compete Agreement
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Finance Agreement
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="#" className="nav-link p-0  ">
+              Freelancer Agreement
+              </a>
+            </li>
+            
+          </ul>
+        </div>
+      </footer>
+    </div>
+    </div>
+
+
+    </>
   )
 }
 
